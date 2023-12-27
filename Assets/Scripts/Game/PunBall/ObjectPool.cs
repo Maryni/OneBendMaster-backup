6bad09ea5eff63abbbb11e586e1888cf14f21076
Andrew <prikolchik574@gmail.com>
2023-12-27 14:10:52 +0200

    removing unused files

--- Assets/Scripts/Game/PunBall/ObjectPool.cs
+++ Assets/Scripts/Game/PunBall/ObjectPool.cs
@@ -14,20 +14,10 @@ public class ObjectPool : MonoBehaviour
 {
     #region Inspector variables
 
-    [Header("Prefabs for Init"),SerializeField] private List<GameObject> prefabsBulletList;
-    [SerializeField] private List<GameObject> prefabsEnemyList;
-    [SerializeField] private List<GameObject> prefabsSpriteList;
-    [Header("Inited objects"),SerializeField] private List<GameObject> exampleBulletList;
-    [SerializeField] private List<GameObject> exampleEnemyList;
-    [SerializeField] private List<GameObject> exampleSpriteList;
-
-    [Header("Transform for pools"),SerializeField] private Transform transformBulletParent;
-    [SerializeField] private Transform transformEnemyParent;
-    [SerializeField] private Transform transformSpriteParent;
-
-    [Header("Count inited object for each type"),SerializeField] private int countBulletsExampleToInit;
-    [SerializeField] private int countEnemyExampleToInit;
-    [SerializeField] private int countSpriteExampleToInit;
+    [Header("Prefabs for Init"), SerializeField] private List<GameObject> prefabsSpriteList;
+    [Header("Inited objects"), SerializeField] private List<GameObject> exampleSpriteList;
+    [Header("Transform for pools"), SerializeField] private Transform transformSpriteParent;
+    [Header("Count inited object for each type"), SerializeField] private int countSpriteExampleToInit;
 
     #endregion Inspector variables
 
@@ -39,57 +29,23 @@ public class ObjectPool : MonoBehaviour
 
     #region properties
 
-    public List<GameObject> PrefabsBulletList => prefabsBulletList;
-    public List<GameObject> PrefabsEnemyList => prefabsEnemyList;
     public List<GameObject> PrefabsSpriteList => prefabsSpriteList;
 
     #endregion properties
-    
+
     #region Unity functions
 
     private void Start()
     {
-       StartCoroutine(Init());
+        StartCoroutine(Init());
     }
 
     #endregion Unity functions
-    
+
     #region public functions
 
     public GameObject GetObjectByType(ObjectType objectType, ElementType elementType)
     {
-        if (objectType == ObjectType.Bullet)
-        {
-           var findedObject = exampleBulletList.Where(x => x.GetComponent<BaseBullet>().ElementType == elementType)
-               .FirstOrDefault(x => !x.activeSelf);
-           if (findedObject == null)
-           {
-               var exampleObject =
-                   exampleBulletList.FirstOrDefault(x => x.GetComponent<BaseBullet>().ElementType == elementType);
-               var newObject = Instantiate(exampleObject, transformBulletParent);
-               exampleBulletList.Add(newObject);
-               newObject.SetActive(true);
-               return newObject;
-           }
-           findedObject.SetActive(true);
-           return findedObject;
-        }
-        if (objectType == ObjectType.Enemy)
-        {
-            var findedObject = exampleEnemyList.Where(x => x.GetComponent<BaseEnemy>().ElementType == elementType)
-                .FirstOrDefault(x => !x.activeSelf);
-            if (findedObject == null)
-            {
-                var exampleObject =
-                    exampleEnemyList.FirstOrDefault(x => x.GetComponent<BaseEnemy>().ElementType == elementType);
-                var newObject = Instantiate(exampleObject, transformEnemyParent);
-                exampleEnemyList.Add(newObject);
-                newObject.SetActive(true);
-                return newObject;
-            }
-            findedObject.SetActive(true);
-            return findedObject;
-        }
         if (objectType == ObjectType.MatchThreeSprite)
         {
             var findedObject = exampleSpriteList.Where(x => x.GetComponentInChildren<BaseMatchThree>().ElementType == elementType)
@@ -124,8 +80,6 @@ public class ObjectPool : MonoBehaviour
     {
         if (isStatsLoaded)
         {
-            InitDefault(prefabsBulletList,countBulletsExampleToInit,transformBulletParent,exampleBulletList);
-            InitDefault(prefabsEnemyList,countEnemyExampleToInit,transformEnemyParent, exampleEnemyList);
             InitDefault(prefabsSpriteList, countSpriteExampleToInit, transformSpriteParent, exampleSpriteList);
         }
         else
@@ -146,7 +100,7 @@ public class ObjectPool : MonoBehaviour
                 obj.SetActive(false);
                 exampleList.Add(obj);
             }
-        } 
+        }
     }
 
     #endregion private functions
